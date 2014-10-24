fd58853bb7efbcb70145084c061e7fb1e8e30638
Benoît Brochier <b.brochier@c2is.fr>
2014-10-24 22:15:12 +0200

    offset management

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Customizable Scroll to hash jQuery plugin
 
 ##Usage
 ```
-	$('a').scrollToHash();
+	$('.trigger').scrollToHash();
 ```
 
 ##1. Options
@@ -15,24 +15,22 @@ All of the options below are available to customize scrollToHash.
 | Variable | Default | Type | Description |
 | :----- | :----- | :----- | :----- |
 | scrollSpeed | 1000 | int | Speed in ms of the scroll |
-| urlRewrite | true | boolean | Rewrite of URL width #target |
-| offsetTop | 0 | int | Distance in px where to stop scroll above the target |
-| offsetBottom | 0 | int | Distance in px where to stop scroll underneath the target |
+| scrollOffset | 0 | int | Distance in px where to stop scroll before or after the target |
+| urlRewrite | true | boolean | Rewrite of URL with #target |
 
 ##2. Default
 
 ```
-	$('').scrollToHash({
-        scrollSpeed 	: 1000,
-        offsetTop 	: 0,
-        offsetBottom 	: 0,
-        urlRewrite	: true
+	$('.trigger').scrollToHash({
+        scrollSpeed     : 1000,
+        scrollOffset 	: 0,
+        urlRewrite	    : true
     });
 ```
 
 ##Todos
 
-- [ ] offset management
+- [x] offset management
 - [x] scroll speed
 - [ ] callback
 - [ ] target si pas de #
@@ -40,4 +38,5 @@ All of the options below are available to customize scrollToHash.
 
 
 ##Sources
-Scroll to hash smoothly : [http://css-tricks.com/snippets/jquery/smooth-scrolling/](http://css-tricks.com/snippets/jquery/smooth-scrolling/)
+- CSS Tricks : [http://css-tricks.com/snippets/jquery/smooth-scrolling/](http://css-tricks.com/snippets/jquery/smooth-scrolling/)
+- jQuery Boilerplate : [http://jqueryboilerplate.com/](http://jqueryboilerplate.com)
--- scrolltohash.js
+++ scrolltohash.js
@@ -1,26 +1,94 @@
-(function($) {
+/*
+ *  Project: JQuery ScrollToHash
+ *  Description: 
+ *  Author: Benoit Brochier
+ *  Version: 0.1
+ */
 
-    $.fn.scrollToHash = function(options) {
+;(function ( $, window, document, undefined ) {
 
-      // Default options
-      var settings = $.extend({
-          scrollSpeed    : 1000,
-          element        : null
-      }, options);
+    // Default options
+    var pluginName = "scrollToHash",
+        defaults = {
+            scrollSpeed   : 1000,
+            target        : null,
+            scrollOffset  : 0,
+        };
 
-      this.click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
+
+    // Plugin constructor
+    function Plugin( element, options ) {
+        this.element = element;
+
+        this.options = $.extend( {}, defaults, options) ;
+
+        this._defaults = defaults;
+        this._name = pluginName;
+
+        this.init();
+    }
+
+    Plugin.prototype = {
+
+        init: function() {
+            var iScrollSpeed = this.options.scrollSpeed,
+                iScrollOffset = this.options.scrollOffset;
+
+            $(this.element).on("click", function(e) {
+              e.preventDefault();
+              if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                  Plugin.prototype.scrollToTarget(target, iScrollSpeed, iScrollOffset);
+                }
+              }
+            });
+        },
+
+        scrollToTarget: function(target, scrollSpeed, scrollOffset) {
             $('html,body').animate({
-              scrollTop: target.offset().top
-            }, settings.scrollSpeed);
+              scrollTop: target.offset().top + scrollOffset
+            }, scrollSpeed);
             return false;
-          }
         }
-      });
 
     };
 
-}(jQuery));
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName,
+                new Plugin( this, options ));
+            }
+        });
+    };
+
+
+
+    // $.fn.scrollToHash = function(options) {
+
+    //   // Default options
+    //   var settings = $.extend({
+    //       scrollSpeed    : 1000,
+    //       element        : null
+    //   }, options);
+
+    //   this.click(function() {
+    //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    //       var target = $(this.hash);
+    //       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    //       if (target.length) {
+    //         $('html,body').animate({
+    //           scrollTop: target.offset().top
+    //         }, settings.scrollSpeed);
+    //         return false;
+    //       }
+    //     }
+    //   });
+
+    // };
+
+}(jQuery, window, document));
